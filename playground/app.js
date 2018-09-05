389b8fe9dd5cc579b4b1289b0511f276bc6f8f9d
Toni Tesori <tt@tesoridesign.com>
2018-09-05 12:18:08 -0400

    Update gitignore

--- playground/app.js
+++ playground/app.js
@@ -1,16 +1,82 @@
-// ES6 Classes
+// deleteNth ([1,1,1,1],2) // return [1,1]
+// deleteNth ([20,37,20,21],1) // return [20,37,21]
+// #3 - [35, 20, 3, 17, 15, 22, 13, 36]
+// #4 - [7, 26, 17, 3, 41, 33, 18, 48]
 
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  greeting() {
-    return `Hello there ${this.firstName} ${this.lastName}.`;
-  }
+function deleteNth(arr, n) {
+  console.log('-----------------------');
+  let newArr = arr.slice(0);
+  // loop through array
+  arr.forEach(num => {
+    let currentCount = 0; //reset current count
+    // loop through array again for comparison
+    arr.forEach((num2, index2) => {
+      // if currentNum equals current number
+      if (num === num2) {
+        currentCount++;
+        // if currentCount greater than allowed
+        if (currentCount > n) {
+          console.log(
+            `Current count for ${num} at ${index2} is ${currentCount}`
+          );
+          // remove from array
+          arr.splice(index2, 1);
+        }
+      }
+    });
+  });
+  return arr;
 }
 
-const mary = new Person('Mary', 'Williams');
-console.log(mary);
-console.log(mary.greeting());
+//console.log(deleteNth([1, 1, 1, 1], 2));
+//console.log(deleteNth([20, 37, 20, 21], 1));
+console.log(
+  deleteNth(
+    [
+      35,
+      20,
+      3,
+      17,
+      35,
+      20,
+      15,
+      17,
+      22,
+      20,
+      22,
+      3,
+      17,
+      22,
+      17,
+      20,
+      13,
+      15,
+      20,
+      13,
+      36,
+      13,
+      13,
+      13,
+      13,
+      13,
+      13,
+      17,
+      15,
+      17,
+      15,
+      36,
+      17,
+      17,
+      17,
+      35,
+      15,
+      17,
+      13,
+      20,
+      13,
+      36,
+      15
+    ],
+    1
+  )
+);
