7a943b3b5024d3d3f2e3be235f2d8bab540290a7
Toni Tesori <tt@tesoridesign.com>
2018-10-16 12:25:17 -0400

    test

--- playground/app.js
+++ playground/app.js
@@ -1,13 +1,6 @@
-const createTipper = tip => {
-  return bill => {
-    return tip * bill;
-  };
-};
+function getMiddle(s) {}
 
-const tip15 = createTipper(0.15);
-console.log(tip15(10));
-console.log(tip15(25));
-
-const tip20 = createTipper(0.2);
-console.log(tip20(10));
-console.log(tip20(25));
+console.log(getMiddle('test'));
+console.log(getMiddle('testing'));
+console.log(getMiddle('middle'));
+console.log(getMiddle('A'));
